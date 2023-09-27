9e06b28cf4e78fcb2273784c1155c8aeff8c68be
hiennguyen727 <hien.d.nguyen1997@gmail.com>
2023-09-27 15:02:06 -0400

    styled and added more features

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ const App = () => {
   return (
     <div>
 
-      <h2>Racer 1</h2>
-      <Stopwatch/>
-      <h2>Racer 2</h2>
-      <Stopwatch/>
-      <h2>Racer 3</h2>
-      <Stopwatch/>
-      <h2>Racer 4</h2>
+      <h2 className='header1'>Stop Watch</h2>
       <Stopwatch/>
       
       
--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import '../App.css'
+
 const Stopwatch = () => {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(0); // time in seconds
   const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
     let intervalId;
     if (isRunning) {
-      intervalId = setInterval(() => setTime(time + 1), 1); 
+      intervalId = setInterval(() => setTime(time + 10), 10); // increment by 10 milliseconds
     }
     return () => clearInterval(intervalId);
   }, [isRunning, time]);
@@ -15,16 +16,25 @@ const Stopwatch = () => {
   const startAndStop = () => {
     setIsRunning(!isRunning);
   };
-  
+
   const reset = () => {
     setTime(0);
   };
 
+  const convertTime = (time) => {
+    const milliseconds = time % 1000; // Calculate milliseconds
+    const seconds = Math.floor((time / 1000) % 60); // Calculate seconds
+    const minutes = Math.floor((time / (1000 * 60)) % 60); // Calculate minutes
+    const hours = Math.floor(time / (1000 * 60 * 60)); // Calculate hours
+
+    return `${hours}:${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}.${milliseconds}`;
+  }
+
   return (
     <div className="stopwatch-container">
-      <p className="stopwatch-time">{time}  millisecondz</p>
-      <div >
-        <button onClick={startAndStop}>
+      <p className="stopwatch-time">{convertTime(time)}</p>
+      <div>
+        <button className="hello" onClick={startAndStop}>
           {isRunning ? "Stop" : "Start"}
         </button>
         <button onClick={reset}>
@@ -36,3 +46,4 @@ const Stopwatch = () => {
 };
 
 export default Stopwatch;
+
